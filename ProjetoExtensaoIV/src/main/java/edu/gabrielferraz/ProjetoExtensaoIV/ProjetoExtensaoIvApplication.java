e3b35bc71df4d26fd214c69b871db98914d123f2
GabrielHFerraz <gabrielsh38s2@gmail.com>
2024-09-04 20:45:09 -0300

    Start Backend
    Criado projeto do backend com Java SpringBoot

---
+++ ProjetoExtensaoIV/src/main/java/edu/gabrielferraz/ProjetoExtensaoIV/ProjetoExtensaoIvApplication.java
@@ -0,0 +1,13 @@
+package edu.gabrielferraz.ProjetoExtensaoIV;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoExtensaoIvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoExtensaoIvApplication.class, args);
+	}
+
+}
